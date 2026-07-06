3d249261aa143804182c2c618fdbe35332be423d
Quoc Khanh Ho <QuocKhanh.Ho@intusurg.com>
2026-07-06 16:32:04 -0700

    finished Lesson3-Validate the first question

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -1,7 +1,27 @@
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
+function isFormValid() {
+    let isValid = true;
+    let q1Response = document.querySelector("#q1").value;
+    let validationFdbk = document.querySelector("#validationFdbk");
+
+    if (q1Response === "") {
+        isValid = false;
+        validationFdbk.textContent = "Question 1 was not answered";
+    }
+
+    return isValid;
+}
+
 function gradeQuiz() {
   console.log("Grading quiz...");
+  document.querySelector("#validationFdbk").textContent = "";
+
+  if (!isFormValid()) {
+    return;
+  }
+
   let q1Response = document.querySelector("#q1").value;
   console.log(q1Response);
 }
+
